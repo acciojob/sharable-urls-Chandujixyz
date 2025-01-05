9454640d7d5c652e06bf26f0f54d7e718f3d36ac
Chandan Gupta <chandugupta030797@gmail.com>
2025-01-06 00:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+ const button=document.getElementById("button");
+    button.addEventListener("click",fun);
+
+    function fun(e){
+        e.preventDefault();
+        
+        const name=document.getElementById("name").value;
+        const year=document.getElementById("year").value;
+
+        const h3=document.getElementById("url");
+        if(name===""&&year===""){
+            alert("please fill the form")
+        }else if(name===""){
+            h3.innerText=`https://localhost:8080/?year=${year}`;
+        }else if(year===""){
+            h3.innerText=`https://localhost:8080/?name=${name}`;
+        }else{
+            h3.innerText=`https://localhost:8080/?name=${name}&year=${year}`;
+        }
+    }
